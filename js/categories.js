1a194412b154ed48491fca551a984bc446f73389
Martin Silva <martinsilva510@gmail.com>
2024-06-29 18:32:08 -0300

    continue category

--- js/categories.js
+++ js/categories.js
@@ -1,3 +1,10 @@
 const bntMen = document.getElementById("category-hombre"),
 btnWoman = document.getElementById("category-mujer");
 
+const showMeMen = ()=>{
+    if (category === hombres) {
+        console.log("hola")
+        
+    }
+}
+bntMen.addEventListener("click",showMeMen())
