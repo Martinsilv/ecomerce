8849ee8df334d1822c6be30e12793a989470a51f
Martin Silva <martinsilva510@gmail.com>
2024-07-08 00:34:10 -0300

    category

--- js/categories.js
+++ js/categories.js
@@ -1,11 +1,2 @@
 const bntMen = document.getElementById("category-hombre"),
-btnWoman = document.getElementById("category-mujer"),
-product = productos.categorie;
-const showMeMen = ()=>{
-
-    if (product === hombres) {
-    
-        
-    }
-}
-bntMen.addEventListener("click",showMeMen())
+btnWoman = document.getElementById("category-mujer")
